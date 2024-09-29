397666aa145833b61bf86d817652e211acc068d3
AdelaFoofur82 <78669385+AdelaFoofur82@users.noreply.github.com>
2024-09-29 14:38:38 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'temporizador-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/rsc/dim.mp3',  // Asegúrate de incluir todos los recursos necesarios para que la PWA funcione sin conexión.
+  '/rsc/gong.wav',
+  '/rsc/tibetan.wav',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;  // Si está en la caché, lo devuelve.
+        }
+        return fetch(event.request);  // Si no está, lo descarga.
+      })
+  );
+});
